ea538d2845cd9ababf33a57052403e6300a34612
Nestornjrz <nestornjrz@hotmail.com>
2017-01-25 15:49:07 -0300

    Se crea la capa de presentancion que va a consumir la API

---
+++ Conta.Web.Contabilidad/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Conta.Web.Contabilidad.Startup))]
+namespace Conta.Web.Contabilidad
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
