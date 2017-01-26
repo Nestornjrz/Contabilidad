ef9808464f4aea79e8d5808ac4398434b5b8bf6d
Nestornjrz <nestornjrz@hotmail.com>
2017-01-26 14:53:29 -0300

    Modificando los nombres de los archivos

--- Conta.Web.Contabilidad/tsScripts/app.module.ts
+++ Conta.Web.Contabilidad/tsScripts/app.module.ts
@@ -1,7 +1,7 @@
 ﻿///<reference path="./../typings/globals/core-js/index.d.ts"/>
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app';
+import { AppComponent } from './app.component';
 
 @NgModule({
     imports: [BrowserModule ],
