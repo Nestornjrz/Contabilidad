f49bfce85a47933c93408c00cabc73010ac174d0
Nestornjrz <nestornjrz@hotmail.com>
2017-02-16 15:11:18 -0300

    Configuracion basica para Hacer funcionar Identity Server 3

---
+++ Conta.Sts.IdentityServer/Config/Scopes.cs
@@ -0,0 +1,22 @@
+﻿using IdentityServer3.Core.Models;
+using System.Collections.Generic;
+
+namespace Conta.Sts.IdentityServer.Config
+{
+    public static class Scopes
+    {
+        public static IEnumerable<Scope> Get()
+        {
+            return new List<Scope>
+                {
+                    new Scope
+                    {
+                        Name = "gallerymanagement",
+                        DisplayName = "Gallery Management",
+                        Description = "Allow the application to manage galleries on your behalf.",
+                        Type = ScopeType.Resource
+                    }
+                };
+        }
+    }
+}
--- Conta.Sts.IdentityServer/Startup.cs
+++ Conta.Sts.IdentityServer/Startup.cs
@@ -1,7 +1,9 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using Conta.Sts.IdentityServer.Config;
+using IdentityServer3.Core.Configuration;
 using Microsoft.Owin;
 using Owin;
+using System;
+using System.Security.Cryptography.X509Certificates;
 
 [assembly: OwinStartup(typeof(Conta.Sts.IdentityServer.Startup))]
 
@@ -12,6 +14,31 @@ namespace Conta.Sts.IdentityServer
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.Map("/identity", idsrvApp =>
+            {
+                var idServerServiceFactory = new IdentityServerServiceFactory()
+                                .UseInMemoryClients(Clients.Get())
+                                .UseInMemoryScopes(Scopes.Get())
+                                .UseInMemoryUsers(Users.Get());
+
+                var options = new IdentityServerOptions
+                {
+                    Factory = idServerServiceFactory,
+                    SiteName = "TripCompany Security Token Service",
+                    IssuerUri = Conta.Sts.Constants.Constants.Conta_IssuerUri,
+                    PublicOrigin = Conta.Sts.Constants.Constants.Conta_STSOrigin,
+                    SigningCertificate = LoadCertificate()
+                };
+
+                idsrvApp.UseIdentityServer(options);
+            });
+        }
+
+        X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                string.Format(@"{0}\certificates\idsrv3test.pfx",
+                AppDomain.CurrentDomain.BaseDirectory), "idsrv3test");
         }
     }
 }
